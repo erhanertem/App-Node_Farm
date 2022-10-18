efe08ab19ef51e63e7b811f3911438d71516a801
Erhan Ertem <eertem@gmail.com>
2022-10-18 13:06:18 +0300

    Dwell on layered/cascaded read and write async operations

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const { Console } = require("console");
-const fs = require("fs");
+const fs = require('fs');
 //NOTE: NodeJS is built around modules which provide distinct functionality. fs stands for file system which prvoides us functions like reading writing deleting files. We load the node functionality to a variable and call pertinent functions built around it.
 
 // //Blocking Way - Synchronous
@@ -11,10 +10,31 @@ const fs = require("fs");
 // fs.writeFileSync("./txt/output.txt", textOut);
 // console.log("File written!");
 
-//Non-blocking Wahy - Asynchronous
-fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
-	console.log(data);
+// //Non-blocking Wahy - Asynchronous
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
+//   console.log(data);
+// });
+// //We do not need to specify text encoding as in the sync version
+// //NOTE: Callback functions used in nodejs functions typically gets (err,data)
+// console.log('Will read file!');
+
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+  //async readFile bears err, data
+  console.log(data1);
+  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+    console.log(data2);
+    fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+      console.log(data3);
+      //Async write operation
+      fs.writeFile(
+        './txt/final.txt', //file to write to
+        `${data2}\n${data3}`, // what to write - simply the content
+        'utf-8', //txt encoding
+        error => {
+          console.log('The file has been written 🧿');
+        } //async writeFile bears ONLY error
+      );
+    });
+  });
 });
-//We do not need to specify text encoding as in the sync version
-//NOTE: Callback functions used in nodejs functions typically gets (err,data)
-console.log("Will read file!");
+console.log('Will read file!');
